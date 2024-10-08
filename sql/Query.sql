60410bcaee1c6efcc08cbb4419e260eab7883bfb
Shreya Ambeti <138755963+shreya-ambeti@users.noreply.github.com>
2024-10-08 19:52:26 +0530

    Add files via upload

---
+++ sql/Query.sql
@@ -0,0 +1,5 @@
+create database employeemanagementsystem;
+use employeemanagementsystem;
+create table login(username varchar(20), password varchar(20));
+insert into login values('admin', '12345');
+select * from employee
